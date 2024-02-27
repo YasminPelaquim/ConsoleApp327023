6b03d758250c918f8a86254affb5a84c18447dca
Usuario <Usuario@CPD02-M06>
2024-02-27 11:47:02 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp327023/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ConsoleApp327023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a sua idade em anos: ");
+            int anos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantos meses se passaram depois do seu aniversário: ");
+            int meses = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantos dias se passaram desde o último mês: ");
+            int dias = int.Parse(Console.ReadLine());
+
+            int anosdias = anos * 365;
+
+            int mesesdias = meses * 30;
+
+            int total = anosdias + mesesdias + dias;
+
+            Console.WriteLine("O total de dias vividos é: " + total);
+
+            Console.ReadKey();
+
+        }
+    }
+}
